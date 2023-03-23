8e1d38a3ec94285acc561ec999bff530c20c1f5e
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-03-22 21:46:21 -0300

    service to seed 650 pokemon to DB

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,15 +3,24 @@ import { Injectable } from '@nestjs/common';
 import { catchError, firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSEED() {
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await firstValueFrom(
       this.httpService
-        .get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
         .pipe(
           catchError((error: AxiosError) => {
             console.error(error.response.data);
@@ -20,12 +29,16 @@ export class SeedService {
         ),
     );
 
-    data.results.forEach(async ({ name, url }) => {
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.map(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      console.log({ name, no });
+      pokemonToInsert.push({ name, no });
     });
 
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed Executed';
   }
 }
