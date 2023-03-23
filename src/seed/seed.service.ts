6675f9d1f826b341267aa662f401f01f0be00305
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-03-22 21:14:38 -0300

    axios and pre seed service

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,31 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async executeSEED() {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .pipe(
+          catchError((error: AxiosError) => {
+            console.error(error.response.data);
+            throw 'An error happened!';
+          }),
+        ),
+    );
+
+    data.results.forEach(async ({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log({ name, no });
+    });
+
+    return data.results;
+  }
+}
